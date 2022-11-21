3d006641885e8bf9bf0b2daf841cd4fe0ed4603c
Jacques AV <jacques.adriaan.venter@gmail.com>
2022-11-21 17:00:19 +0100

    Implemented Simple Jump
    Includes small restructure to the JellyCat prototypes hierarchy, reducing issues with the forward changing.

--- Assets/Scripts/JellyCatController.cs
+++ Assets/Scripts/JellyCatController.cs
@@ -1,37 +1,43 @@
 using UnityEngine;
 
+/// <summary>
+/// Script that manages the movement of the JellyCat player.
+/// </summary>
 public class JellyCatController : MonoBehaviour
 {
-    [SerializeField]private CharacterController controller;
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private float movementSpeed = 2.0f;
+    [SerializeField] private float jumpHeight = 3.0f;
+    [SerializeField] [Range(0, 10)] private float gravityStrength = 9.81f;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    private float playerSpeed = 2.0f;
-    private float jumpHeight = 1.0f;
-    private float gravityValue = -9.81f;
 
+    /// <summary>
+    /// Called once every frame.
+    /// </summary>
     private void Update()
     {
+        // Set & reset relevant data for the movement.
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
         }
 
+        // Handle XZ movement.
         var move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * (Time.deltaTime * playerSpeed));
-
+        controller.Move(move * (Time.deltaTime * movementSpeed));
         if (move != Vector3.zero)
         {
             gameObject.transform.forward = move;
         }
 
-        // Changes the height position of the player..
+        // Handle the player jumping & height position.
         if (Input.GetButtonDown("Jump") && groundedPlayer)
         {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * gravityStrength);
         }
-
-        playerVelocity.y += gravityValue * Time.deltaTime;
+        playerVelocity.y -= gravityStrength * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
     }
 }
