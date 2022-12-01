57fae1c5fe99012d27338869d7555540252a006b
Ryan <stander.ryan@gmail.com>
2022-12-01 22:12:31 +0100

    CameraSwapper

---
+++ Assets/Scripts/Camera/SwapCameraOnEnter.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace Camera
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public class SwapCameraOnEnter : MonoBehaviour
+    {
+        [SerializeField] private GameObject cameraToSwapTo;
+        [SerializeField] private GameObject[] camerasToDisable;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.gameObject.CompareTag("Player")) return;
+            
+            Debug.Log("player enter");
+            
+            foreach (var cameraToDisable in camerasToDisable)
+            {
+                cameraToDisable.SetActive(false);
+            }
+            cameraToSwapTo.SetActive(true);
+        }
+    }
+}
